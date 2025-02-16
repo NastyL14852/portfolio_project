e588e12ee2b9b880e0e15e2828829e8163e296ed
NastyL1485 <peretyatkoanastasija@yandex.by>
2025-02-16 17:27:35 +0300

    fourth (sidebar, main)

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,30 @@ function App() {
     <div className="App">
       <header className="App-header">
         <div className='name-text'>
-          <div className='my-name'><h1>Anastasiya Peretyatko</h1></div>
-          <div className='project-number'><h2>Project 3</h2></div>
+          <div className='my-name'><p>Anastasiya Peretyatko</p></div>
+          <div className='project-number'><p>Project 3</p></div>
         </div>
-        <button className='header-button'><img src='/home.svg' alt='Home' className='button-img'></img></button>
-        <button className='header-button'><img src='/projects.svg' alt='Projects' className='button-img'></img></button>
-        <button className='header-button'><img src='/about.png' alt='About me' className='button-img'></img></button>
-        <button className='header-button'><img src='/contacts.png' alt='Contacts' className='button-img'></img></button>
+        <div className='box-button'>
+          <button className='header-button'><img src='/home.svg' alt='Home' className='button-img'></img></button>
+          <button className='header-button'><img src='/projects.svg' alt='Projects' className='button-img'></img></button>
+          <button className='header-button'><img src='/about.png' alt='About me' className='button-img'></img></button>
+          <button className='header-button'><img src='/contacts.png' alt='Contacts' className='button-img'></img></button>
+        </div>
       </header>
       <main className="App-main">
-        <p className='main-p'>Main</p>
+        <article className='about-me'></article>
+        <article className='skills'></article>
+        <article className='projects'>
+          <section className='project-1'></section>
+          <section className='project-2'></section>
+          <section className='project-3'></section>
+        </article>
       </main>
       <aside className='App-sidebar'>
-        <p className='Sidebar-p'>Sidebar</p>
+        <div className='my-photo'>
+          <img src='/my-photo.jpg' alt='Me :)' className='my-photo-img'></img>
+        </div>
+        <div className='sidebar-box'></div>
       </aside>
       <footer className='App-footer'>
         <p className='Footer-p'>© 2025 All Rights Reserved by ib-themes.</p>
